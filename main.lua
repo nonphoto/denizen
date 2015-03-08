4dc62c0f08ac0ab5c63775182bcce2036b3f9312
wurflen <jonaskluebbers@gmail.com>
2015-03-08 20:25:05 +0100

    Started working on buttons as an alternative to draggables

--- main.lua
+++ main.lua
@@ -15,9 +15,17 @@ ui = require("ui")
 
 require("vector")
 
-local global = {}
+function getMouse()
+   return vector(love.mouse.getPosition())
+end
 
-global.mouse = vector()
+local menu = {}
+menu.modes = {
+   draw = ui.button(),
+   edit = ui.button(),
+   play = ui.button()
+}
+menu.currentMode = "draw"
 
 function love.load()
    love.graphics.setLineWidth(2)
@@ -34,7 +42,12 @@ function love.mousepressed(x, y, button)
 	 love.mousepressed(x, y, 'r')
 	 return
       end
-      lineStart = vector(x, y)
+      local p = terrain:pointInRadius(getMouse(), 7)
+      if p then
+	 lineStart = p
+      else
+	 lineStart = vector(x, y)
+      end
       lineEnd = vector(x, y)
    end
    ui.mousepressed(x, y, button)
@@ -61,10 +74,20 @@ function love.update(dt)
    if love.keyboard.isDown("left")  then player:move(-v,  0) end
    if love.keyboard.isDown("right") then player:move( v,  0) end
    if love.mouse.isDown("l") then
-      local mouse = vector(love.mouse.getPosition())
-      lineEnd = mouse
-      local x = terrain:pointInRadius(mouse, 7)
-      if x then lineEnd = x end
+      local p = terrain:pointInRadius(getMouse(), 7)
+      if p then
+	 lineEnd = p
+      else
+	 lineEnd = getMouse()
+      end
+   end
+
+   if menu.modes.draw(vector(30, 30), 15) then
+      menu.mode = "draw"
+   elseif menu.modes.edit(vector(75, 30), 15) then
+      menu.mode = "edit"
+   elseif menu.modes.play(vector(120, 30), 15) then
+      menu.mode = "play"
    end
    
    terrain:update()
--- ui.lua
+++ ui.lua
@@ -21,29 +21,29 @@ function ui.mousereleased(x, y, button)
    if button == 'r' then
       ui.active = nil
    end
-end
+end   
 
-function ui.handle()
-      -- sneaky trick to give widgets their own identity
+function ui.button()
+   -- sneaky trick to give widgets their own identity
    local widget = {}
    return function(position, radius)
       local mouse = vector(love.mouse.getPosition())
       if vector.lenSq(position - mouse) < radius * radius then
 	 ui.drawWidget(function()
 	       love.graphics.setColor(255, 0, 0)
-				    love.graphics.circle("fill", position.x, position.y, radius, 10)
-			      end)
-			      ui.hover = widget
-			   else
-			      ui.drawWidget(function()
-				    love.graphics.setColor(80, 150, 255)
-				    love.graphics.circle("fill", position.x, position.y, radius, 10)
-			      end)
-			   end
-
-			   if widget == ui.active then
-			      return mouse
-			   end
+	       love.graphics.circle("fill", position.x, position.y, radius, 20)
+	 end)
+	 ui.hover = widget
+      else
+	 ui.drawWidget(function()
+	       love.graphics.setColor(80, 150, 255)
+	       love.graphics.circle("fill", position.x, position.y, radius, 20)
+	 end)
+      end
+      
+      if widget == ui.active then
+	 return mouse
+      end
    end
 end
 
